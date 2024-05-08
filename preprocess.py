035c31692acefa401846ed58a633df4954ae5fa6
Henry_Chen <90209480+Heng-Henry@users.noreply.github.com>
2024-05-09 02:27:43 +0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -21,8 +21,6 @@ def remove_stopwords(text: str) -> str:
 
 def preprocessing_function(text: str) -> str:
     preprocessed_text = remove_stopwords(text)
-    # TO-DO 0: Other preprocessing function attemption
-    # Begin your code
 
     preprocessed_text = text.lower()   # 將輸入都轉成小寫
     preprocessed_text = "".join([char for char in preprocessed_text if (char not in string.punctuation)]) # 把標點符號刪掉
@@ -35,11 +33,8 @@ def preprocessing_function(text: str) -> str:
     preprocessed_text = ' '.join(stemmed)  # join回preprocessed_text
     preprocessed_text = preprocessed_text.replace("<br />", " ")   # 刪掉換行符號
 
-    # End your code
+
     return preprocessed_text
 
 
-# UNIT TEST
-print(preprocessing_function("Here is a dog."))
-print(preprocessing_function("The movie is quite good."))
-print(preprocessing_function("I love learning math."))
+
